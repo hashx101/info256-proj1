9488b4024f8ae4401640d4d2cfbcee42f3f8a4e1
ramatevish <alexandermatevish@gmail.com>
2013-10-08 16:16:59 -0700

    Wrote a basic parser to handle tagged reviews, basic POS tagger.

---
+++ parse.py
@@ -0,0 +1,85 @@
+from collections import namedtuple
+import re
+import pprint
+
+
+class Feature(object):
+
+    def __init__(self, feature='', sign='+', magnitude=0, tags=[]):
+        self.feature = feature
+        self.sign = int(sign + '1')
+        self.magnitude = int(magnitude)
+        self.tags = tags
+        self.setTags(tags)
+
+    def __repr__(self):
+        return '<Feature>[{}: sign: {}, magnitude: {}, tags: {}]'.format(self.feature,
+                                                                self.sign,
+                                                                self.magnitude,
+                                                                self.tags)
+
+    def setTags(self, tags):
+        self.inSentence = True if 'u' in tags else False
+        self.containsSuggestion = True if 's' in tags else False
+        self.containsCompetingBrand = True if 'cc' in tags else False
+        self.containsSameBrand = True if 'cs' in tags else False
+
+
+class TaggedSentence(object):
+
+    def __init__(self, sentence, features=[]):
+        self.features = features
+        self.sentence = sentence
+
+    def __repr__(self):
+        return '<TaggedSentence>[features: {}, sentence:{}]'.format(self.features,
+                                                                    self.sentence)
+
+TAG_PAT = re.compile("\[([upsc]+)+\]")
+FEATURE_PAT = re.compile("(?P<feature>.*?)\[(?P<sign>[+-])(?P<magnitude>[0-9]+)\]")
+
+def parse_file(filepath):
+    f = open(filepath)
+    fr = f.read()
+    f.close()
+
+    reviews = fr.split('[t]')
+    for i, review in enumerate(reviews):
+        strippedReview = []
+        for line in review.split('\r\n'):
+            line = line.strip()
+            if len(line) > 0:
+                strippedReview.append(line)
+        reviews[i] = strippedReview
+    reviews = filter(lambda l: len(l) > 0, reviews)
+
+    parsedReviews = []
+    for rawReview in reviews:
+        review = []
+        for line in rawReview:
+            review.append(parse_line(line))
+        parsedReviews.append(review)
+    return parsedReviews
+
+def parse_line(line):
+    metadata, sentence = line.split('##', 1)
+    metadata, sentence = metadata.split(','), sentence.strip()
+    rawFeaturesList = map(lambda c: re.search(FEATURE_PAT, c), metadata)
+    tagsList = map(lambda c: re.findall(TAG_PAT, c), metadata)
+    assert(len(featuresList) == len(tagsList)) # would imply shoddy regexes
+    featureList = []
+    for feature, tags in zip(rawFeaturesList, tagsList):
+        if feature:
+            featureList.append(Feature(feature.group('feature'),
+                                       feature.group('sign'),
+                                       feature.group('magnitude'),
+                                       tags))
+    return TaggedSentence(sentence, fL)
+
+def main(f='/home/alexm/product_data_training_heldout/training/Diaper Champ.txt'):
+    parse_file(f)
+    #parse_line("costly[-2][u][cc],derp[-1][cs][p]## Too costly and not efficient if you ask me!")
+
+if __name__ == "__main__":
+    main('/home/alexm/product_data_training_heldout/training/Diaper Champ.txt')
+
---
+++ tagger.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.corpus import brown
+from cPickle import dump, load
+import os
+
+
+def build_tagger():
+	brown_tagged_sents = brown.tagged_sents()
+	t0 = nltk.DefaultTagger('NN')
+	t1 = nltk.UnigramTagger(brown_tagged_sents, backoff=t0)
+	t2 = nltk.BigramTagger(brown_tagged_sents, backoff=t1)
+	with open('tagger.pickle', 'wb') as f:
+		dump(t2, f, -1)
+	return t2
+
+def tagger():
+	if os.path.exists('tagger.pickle'):
+		with open('tagger.pickle', 'rb') as f:
+			tagger = load(f)
+	else:
+		tagger = build_tagger()
+	return tagger
+
+if __name__ == "__main__":
+	tagger = tagger()
+
+	print tagger.evaluate(brown.tagged_sents(categories='news'))
+	print tagger.evaluate(brown.tagged_sents(categories='reviews'))
